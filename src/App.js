cc00dac6ac83ba6157464a62e04fc8c46b065000
ashikasoni5404 <ashikasoni5858@gmail.com>
2024-03-03 00:43:53 +0530

    kitchen section

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Kitchen from './component/kitchen/Kitchen';
+
+function App() {
+  return (
+    <div className="App">
+    <Kitchen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/kitchen/Kitchen.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+const Kitchen = () => {
+    return (
+        <div>
+            <div class="card-group">
+                <div class="card">
+                    <a className="kit_tag" href="#" >
+                        <img  className="kit-img"  src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/ec6cf773ae886bd7.jpg?q=20" class="card-img-top" alt="..." />
+                    </a>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img className="kit-img"  src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/22e305bf45e2e1a2.jpeg?q=20" class="card-img-top" alt="..." />
+                    </a>
+                </div>
+
+                <div class="card">
+                    <a href="#">
+                        <img  className="kit-img" src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/c3146ff3077e7744.jpg?q=20" class="card-img-top" alt="..." />
+                    </a>
+                </div>
+                <div class="card">
+                    <a href="#">
+                        <img src="https://rukminim2.flixcart.com/fk-p-flap/520/280/image/ec6cf773ae886bd7.jpg?q=20" class="card-img-top" alt="..." />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Kitchen;

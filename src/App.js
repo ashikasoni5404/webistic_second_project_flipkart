82a89e08ddf071dabbb5a1814626804bf5e3aad8
ashikasoni5404 <ashikasoni5858@gmail.com>
2024-03-03 14:05:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Kitchen from './component/kitchen/Kitchen';
 import Slider from './component/slider/Slider';
+import Stories from './component/stories/Stories';
 
 function App() {
   return (
     <div className="App">
-   <Slider/>
 
     </div>
   );
---
+++ src/component/stories/Stories.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import "./stories.css"
+
+const Stories = () => {
+    return (
+        <div className="story">
+            <h1 >TOP STORIES : BRAND DIRECTORY</h1>
+            <br />
+            <p className="s_head">MOST SEARCH FOR ON FLIPKART :
+                <span><a href="" className="p_ac">fresh flower delivery </a> </span>
+                <span ><a href="" className="p_ac"> i phone 15 </a></span>
+                <span><a href="" className="p_ac">i phone 15 plus </a></span>
+                <span ><a href="" className="p_ac"> i phone pro </a></span>
+                <span ><a href="" className="p_ac"> i phone 15 pro max </a></span>
+                <span><a href="" className="p_ac">vivo 100 </a></span>
+                <span ><a href="" className="p_ac">spoyl store </a></span>
+                <span ><a href="" className="p_ac"> samsung flip5</a></span>
+                <span ><a href="" className="p_ac"> samsung fold5</a></span>
+                <span ><a href="" className="p_ac">flipkart asix band super elite card </a></span>
+                <span ><a href="" className="p_ac"> 5G mobile phone</a></span>
+                <span ><a href="" className="p_ac"> primebook laptops</a></span>
+                <span ><a href="" className="p_ac">moto edge 40 </a></span>
+                <span ><a href="" className="p_ac">grievance redressal</a></span>
+                <span ><a href="" className="p_ac"> oppo reno7 pro</a></span>
+                <span ><a href="" className="p_ac"> iphone 13 </a></span>
+                <span ><a href="" className="p_ac"> help center</a></span>
+                <span ><a href="" className="p_ac">track order </a></span>
+                <span ><a href="" className="p_ac"> manage order</a></span>
+                <span ><a href="" className="last">Return Order. </a></span>
+
+               
+            </p>
+
+            <p className="s_head">MOBILES :
+                <span><a href="" className="p_ac">samsung galasy s24 g </a> </span>
+                <span><a href="" className="p_ac"> samsung galasy s24+ g</a> </span>
+                <span><a href="" className="p_ac"> i phone 24 64 gb</a> </span>
+                <span><a href="" className="p_ac"> i phone 12 pro 512 gb</a> </span>
+                <span><a href="" className="p_ac"> i phone 12 128 gb</a> </span>
+                <span><a href="" className="p_ac"> vivo y15</a> </span>
+                <span><a href="" className="p_ac"> samsung galaxy s21 fe 5g </a> </span>
+                <span><a href="" className="p_ac"> infinix hot 30 i  </a> </span>
+                <span><a href="" className="p_ac">samsung galasy s24 g </a> </span>
+                <span><a href="" className="p_ac"> samsung galasy s24+ g</a> </span>
+                <span><a href="" className="p_ac"> i phone 24 64 gb</a> </span>
+                <span><a href="" className="p_ac"> i phone 12 pro 512 gb</a> </span>
+                <span><a href="" className="p_ac"> i phone 12 128 gb</a> </span>
+                <span><a href="" className="p_ac"> vivo y15</a> </span>
+                <span><a href="" className="p_ac"> samsung galaxy s21 fe 5g </a> </span>
+                <span><a href="" className="p_ac"> infinix hot 30 i  </a> </span>
+                <span><a href="" className="last"> vivo mobile.</a> </span>
+                
+            </p>
+
+            <p className="s_head">CAMERA :
+                <span><a href="" className="p_ac"> go pro action camera </a> </span>
+                <span><a href="" className="p_ac"> nikon camera</a> </span>
+                <span><a href="" className="p_ac"> canon camera</a> </span>
+                <span><a href="" className="p_ac"> sony camera</a> </span>
+                <span><a href="" className="p_ac"> canon DSLR</a> </span>
+                <span><a href="" className="last">nikon DSLR. </a> </span>
+               
+            </p>
+
+            <p className="s_head">FOOTWEAR :
+                <span><a href="" className="p_ac"> adidas shoes</a> </span>
+                <span><a href="" className="p_ac"> reebok shoes</a> </span>
+                <span><a href="" className="p_ac"> nike shoes</a> </span>
+                <span><a href="" className="p_ac"> puma shoes</a> </span>
+                <span><a href="" className="p_ac"> bata shoes</a> </span>
+                <span><a href="" className="p_ac"> woodland shoes</a> </span>
+                <span><a href="" className="p_ac"> boots shoes</a> </span>
+                <span><a href="" className="p_ac"> skechers shoes</a> </span>
+                <span><a href="" className="p_ac"> sneaker shoes</a> </span>
+                <span><a href="" className="p_ac"> women shoes</a> </span>
+                <span><a href="" className="p_ac"> women boots</a> </span>
+                <span><a href="" className="p_ac"> sport shoes</a> </span>
+                <span><a href="" className="p_ac"> looto  shoes</a> </span>
+                <span><a href="" className="p_ac"> casual shoes</a> </span>
+                <span><a href="" className="p_ac"> asics shoes</a> </span>
+                <span><a href="" className="p_ac"> formal  shoes</a> </span>
+                <span><a href="" className="last"> sandals.</a> </span>
+            </p>
+            <br />
+            <br />
+            <div>
+                <h1>Flipkart: The One-stop Shopping Destination</h1>
+                <br />
+                <p className="p_content">
+                    E-commerce is revolutionizing the way we all shop in India. Why do you want to hop from one store to another in search of the latest phone when you can find it on the Internet in a single click? Not only mobiles. Flipkart houses everything you can possibly imagine, from trending electronics like laptops, tablets, smartphones, and mobile accessories to in-vogue fashion staples like shoes, clothing and lifestyle accessories; from modern furniture like sofa sets, dining tables, and wardrobes to appliances that make your life easy like washing machines, TVs, ACs, mixer grinder juicers and other time-saving kitchen and small appliances; from home furnishings like cushion covers, mattresses and bedsheets to toys and musical instruments, we got them all covered. You name it, and you can stay assured about finding them all here. For those of you with erratic working hours, Flipkart is your best bet. Shop in your PJs, at night or in the wee hours of the morning. This e-commerce never shuts down.
+                    <br /> <br />
+                    What's more, with our year-round shopping festivals and events, our prices are irresistible. We're sure you'll find yourself picking up more than what you had in mind. If you are wondering why you should shop from Flipkart when there are multiple options available to you, well, the below will answer your question.
+                </p>
+            </div>
+            <div>
+                <br/>
+                <br />
+                <h3>FLIPKART PLUS</h3>
+                <p className="p_content">
+                <br/>
+                    A world of limitless possibilities awaits you - Flipkart Plus was kickstarted as a loyalty reward programme for all its regular customers at zero subscription fee. All you need is 500 supercoins to be a part of this service. For every 100 rupees spent on Flipkart order, Plus members earns 4 supercoins &amp; non-plus members earn 2 supercoins. Free delivery, early access during sales and shopping festivals, exchange offers and priority customer service are the top benefits to a Flipkart Plus member. In short, earn more when you shop more!
+                    What's more, you can even use the Flipkart supercoins for a number of exciting services, like: An annual Zomato Gold membership
+                    <br/>
+                    <br/>An annual Hotstar Premium membership
+                    <br/>6 months Gaana plus subscription
+                    <br/>Rupees 550 instant discount on flights on ixigo
+                    <br/> Check out https://www.flipkart.com/plus/all-offers for the entire list. Terms and conditions apply.
+                </p>
+                <br/><br />
+                <h3>NO COST EMI</h3>
+                <br />
+                <p className="p_content">
+                In an attempt to make high-end products accessible to all, our No Cost EMI plan enables you to shop with us under EMI, without shelling out any processing fee. Applicable on select mobiles, laptops, large and small appliances, furniture, electronics and watches, you can now shop without burning a hole in your pocket. If you've been eyeing a product for a long time, chances are it may be up for a no cost EMI. Take a look ASAP! Terms and conditions apply.
+                </p>
+                <br/><br />
+                <h3>EMI ON DEBIT CARD</h3>
+                <br />
+                <p className="p_content">
+                Did you know debit card holders account for 79.38 crore in the country, while there are only 3.14 crore credit card holders? After enabling EMI on Credit Cards, in another attempt to make online shopping accessible to everyone, Flipkart introduces EMI on Debit Cards, empowering you to shop confidently with us without having to worry about pauses in monthly cash flow. At present, we have partnered with Axis Bank, HDFC Bank, State Bank of India and ICICI Bank for this facility. More power to all our shoppers! Terms and conditions apply. 
+                 </p>
+            </div>
+
+        </div>
+    )
+}
+export default Stories;
